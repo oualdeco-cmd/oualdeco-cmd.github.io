5d230a97498d51aa68cfc17bd4a898fe3d0f39fa
oualdeco-cmd <oualdeco@gmail.com>
2026-09-06 20:44:28 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'oualdeco-v1';
+
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                return cache.addAll(FILES_TO_CACHE);
+            })
+    );
+
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+    event.waitUntil(
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(
+                cacheNames
+                    .filter(function(cacheName) {
+                        return cacheName !== CACHE_NAME;
+                    })
+                    .map(function(cacheName) {
+                        return caches.delete(cacheName);
+                    })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fetch(event.request)
+            .catch(function() {
+                return caches.match(event.request);
+            })
+    );
+});
